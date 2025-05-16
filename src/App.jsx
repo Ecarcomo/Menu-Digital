37d3babb2b9d0844a7a10bca6ea333a9c4990d69
Emmanuel Carcomo <emmanuelcarcomo@gmail.com>
2025-05-16 02:08:10 -0300

    Update del workspace

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
 import { useEffect,useState } from 'react'
 import './App.css'
+import {obtenerDatosDeHoja} from './functions/functions.js'
 
 function App() {
   const [menu, setMenu] = useState({});
+  const [menuGS, setMenuGS] = useState({});
   const [bgImage, setBgImage] = useState("");
   const [logoImage, setLogoImage] = useState("");
 
   useEffect(() => {
-    fetch("/menu.json") // El archivo JSON debe estar en public/
-      .then((res) => res.json())
-      .then((data) => setMenu(data));
 
     // Imagen de fondo personalizable (ejemplo fijo, puedes adaptarlo a un input)
     setBgImage("./assets/background.png");
     setLogoImage("./assets/logo.png");
+
+
+    /*fetch("./menu.json") // El archivo JSON debe estar en public/
+      .then((res) => res.json())
+      .then((data) => {
+        setMenu(data);
+        console.log('menu:',data);
+      });*/
+
     
+    
+    obtenerinfo();
+ 
   }, []);
+
+  const obtenerinfo = async () => {
+    obtenerDatosDeHoja('https://docs.google.com/spreadsheets/d/e/2PACX-1vRz9tuezMxV4YtWxMi8oEg2N2sTuH8fK9mhpNaWsTK_51zQdKPSuRDITWLeQYr3Mi36AzGJ9Kl4RLZL/pub?gid=0&single=true&output=tsv')
+    .then((data) => {
+      setMenuGS(data);
+      console.log('menuGS:',data);
+    });
+  }
 
   return (
     <div
@@ -35,11 +54,11 @@ function App() {
         <section key="menu-dia" className="bg-white/80 p-4 m-4 rounded-2xl shadow-md">
           <h2 className="text-2xl font-bold mb-4 text-center">Menu Del Dia</h2>
           <ul className="grid grid-cols-1 sm:grid-cols-2 gap-6 p-4"> 
-          {  Object.entries(menu).map(([category, items]) => 
+          {  Object.entries(menuGS).map(([category, items]) => 
           items.map((item, index) => (
-            (item['menu-ppal'] === "si" && 
+            (item['menu-ppal'] === "si" && item['available']== "si" &&
             <li key={index} className="flex items-start justify-between gap-4 px-10 w-full h-full bg-white py-5 rounded-md place-self-center">
-              <img src={'data:image/jpeg;base64,'+item.image} alt={item.title} className="w-20 h-20 object-cover rounded-md" />
+              <img src={item.image} alt={item.title} className="w-20 h-20 object-cover rounded-md" />
               <div className="flex-1">
                 <span className="bg-white p-1 rounded-md float-right font-bold text-xs uppercase">{category}</span>
                 <div className="font-bold italic text-lg">{item.title}</div>
@@ -67,44 +86,49 @@ function App() {
           </ul>
         </section>
       <div className="grid grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 gap-6 p-4 ">
-      {Object.entries(menu).map(([category, items]) => (
+      {Object.entries(menuGS).map(([category, items]) => (
         
        
        <section key={category} className="bg-white/80 p-4 rounded-2xl shadow-md">
             <h2 className="text-2xl font-bold mb-4 text-center">{category}</h2>
             <ul className="space-y-4">
-              {items.map((item, index) => (
-                <li
-                  key={index}
-                  className="flex items-start justify-between gap-4 border-b last:border-b-0 pb-2"
-                >
-                  {item.image && (
-                    <img
-                      src={'data:image/jpeg;base64,'+item.image}
-                      alt={item.title}
-                      className="w-20 h-20 object-cover rounded-md"
-                    />
-                  )}
-                  <div className="flex-1">
-                    <div className="font-bold italic text-lg">{item.title}</div>
-                    <div className="text-sm text-gray-500">{item.description}</div>
-                    <div className="flex flex-row ">
-                      {item.discountPrice ? (
-                        <>
-                          <div className="ml-auto line-through text-gray-500 mr-2">
-                            ${item.price.toFixed(2)}
+              {
+                items.map((item, index) => (
+                  ( item['available']== "si" && 
+                    (<li
+                        key={index}
+                        className="flex items-start justify-between gap-4 border-b last:border-b-0 pb-2"
+                      >
+                        {item.image && (
+                          <img
+                            src={item.image}
+                            alt={item.title}
+                            className="w-20 h-20 object-cover rounded-md"
+                          />
+                        )}
+                        <div className="flex-1">
+                          <div className="font-bold italic text-lg">{item.title}</div>
+                          <div className="text-sm text-gray-500">{item.description}</div>
+                          <div className="flex flex-row ">
+                            {item.discountPrice ? (
+                              <>
+                                <div className="ml-auto line-through text-gray-500 mr-2">
+                                  ${item.price.toFixed(2)}
+                                </div>
+                                <div className="text-green-600 font-semibold">
+                                  ${item.discountPrice.toFixed(2)}
+                                </div>
+                              </>
+                            ) : (
+                              <div className="ml-auto font-semibold">${item.price.toFixed(2)}</div>
+                            )}
                           </div>
-                          <div className="text-green-600 font-semibold">
-                            ${item.discountPrice.toFixed(2)}
-                          </div>
-                        </>
-                      ) : (
-                        <div className="ml-auto font-semibold">${item.price.toFixed(2)}</div>
-                      )}
-                    </div>
-                  </div>
-                </li>
-              ))}
+                        </div>
+                      </li>
+                    )
+                  )
+                ))
+              }
             </ul>
           </section>
         ))}
---
+++ src/functions/functions.js
@@ -0,0 +1,93 @@
+export async function obtenerDatosDeHoja(urlPublicaCSV) {
+// urlPublicaCSV = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ4x5g7k3z6Z8J9l0G1q5
+  //Asegurate de que la URL sea pública y accesible
+  try {
+    const respuesta = await fetch(urlPublicaCSV);
+    if (!respuesta.ok) {
+      throw new Error(`Error al obtener los datos: ${respuesta.status}`);
+    }
+    const textoCSV = await respuesta.text();
+    const jsonData = tsvToJson(textoCSV);
+
+    let jsonDataTranslated ={}
+    for (const [category, value] of Object.entries(jsonData)) {
+      jsonDataTranslated[category] = value.map(item => {
+                                            const newItem = {};
+                                            for (const [k, v] of Object.entries(item)) {
+                                              newItem[translateToEspecificKey(k)] = v;
+                                            }
+                                            return newItem;
+                                });
+    }
+
+    let jsonDataFinal = {};
+    for (const [category, value] of Object.entries(jsonDataTranslated)) {
+      jsonDataFinal[category] = value.map(item => {
+        const newItem = {};
+        for (const [k, v] of Object.entries(item)) {
+          if (v === undefined || v === null || v === "") {
+            newItem[k] = null;
+          }
+          else if (!isNaN(v)) { 
+            newItem[k] = parseFloat(v);
+          }
+          else{
+            newItem[k] = v;
+          }
+        }
+        return newItem;
+      });
+    }
+
+    return jsonDataFinal;
+  } catch (error) {
+    console.error('Hubo un problema al obtener o procesar los datos:', error);
+    return null;
+  }
+}
+
+function tsvToJson(tsv) {
+  const lineas = tsv.trim().split('\n');
+  const encabezados = lineas[0].split('\t');
+  const resultado = {};
+
+  for (let i = 1; i < lineas.length; i++) {
+    const valores = lineas[i].split('\t');
+    const item = {};
+    let categoria = "";
+
+    for (let j = 0; j < encabezados.length; j++) {
+      const clave = encabezados[j].trim();
+      const valor = valores[j]?.trim() || "";
+      if (clave === "Categoría") {
+        categoria = valor;
+      } else {
+        item[clave] = valor;
+      }
+
+    }
+
+    if (!resultado[categoria]) {
+      resultado[categoria] = [];
+    }
+
+    resultado[categoria].push(item);
+  }
+
+  return resultado;
+}
+
+function  translateToEspecificKey(text) {
+  const translations = {
+    "Categoría": "category",
+    "Título": "title",
+    "Descripción": "description",
+    "Precio": "price",
+    "PrecioDescuento": "discountPrice",
+    "MenuPrincipal": "menu-ppal",
+    "RutaImagen": "image",
+    "Disponible": "available"
+  };
+
+  return translations[text] || text;
+}
